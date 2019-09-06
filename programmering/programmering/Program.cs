56d3ae49adfa38f1970281922880f998738f1b4d
Malik <malo02002@utb.vaxjo.se>
2019-09-06 09:41:37 +0200

    börjat

--- programmering/programmering/Program.cs
+++ programmering/programmering/Program.cs
@@ -6,7 +6,13 @@ namespace programmering
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int tal;
+            Console.Write("ange ett heltal: ");
+            tal = Convert.ToInt32(Console.ReadLine());
+            if (tal % 2 == 0)
+                Console.WriteLine("talet är jämnt");
+            else
+                Console.WriteLine("talet är udda");
 
         }
     }
