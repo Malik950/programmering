4067d2945f06cf37db0c8d9b3fc189d3b391f171
Malik <malo02002@utb.vaxjo.se>
2019-09-10 13:51:22 +0200

    ändrade kod

--- programmering/programmering/Program.cs
+++ programmering/programmering/Program.cs
@@ -14,7 +14,7 @@ namespace programmering
             Console.WriteLine("gissa ett tal mellan 1 och 100");
             tal = Convert.ToInt32(Console.ReadLine());
             
-            string gissning = Console.ReadLine();
+           
             if (tal == slump_tal)
                 Console.WriteLine("talet är rätt grattis");
             else
