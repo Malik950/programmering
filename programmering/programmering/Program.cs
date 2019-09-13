eacf1188f2155ec5d91a80f72304043b9c0b3450
alban Islami <alban2001@hotmail.com>
2019-09-13 09:08:34 +0200

    jag är klar med koden

--- programmering/programmering/Program.cs
+++ programmering/programmering/Program.cs
@@ -7,19 +7,33 @@ namespace programmering
         static void Main(string[] args)
         {
             Random randomerare = new Random();
-            int slump_tal = randomerare.Next(1, 100);
+            int slump_tal = randomerare.Next(100);
 
-            int tal;
+            bool Vinna = false;
+            while (Vinna == false)
+            {
 
-            Console.WriteLine("gissa ett tal mellan 1 och 100");
-            tal = Convert.ToInt32(Console.ReadLine());
-            
-           
-            if (tal == slump_tal)
-                Console.WriteLine("talet är rätt grattis");
-            else
-                Console.WriteLine("talet är fel");
+                int tal;
 
-        }
+                Console.WriteLine("gissa ett tal mellan 1 och 100");
+                tal = Convert.ToInt32(Console.ReadLine());
+
+
+                if (tal == slump_tal)
+                {
+                    Vinna = true;
+                    Console.WriteLine("talet är rätt grattis");
+                }
+
+                else if (tal > slump_tal)
+                {
+                    Console.WriteLine("talet är för högt");
+                }
+                else if (tal < slump_tal)
+                {
+                    Console.WriteLine("talet är för lågt");
+                }
+            }
+        }     
     }
 }
